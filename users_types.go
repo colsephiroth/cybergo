efbd2b3e6644726482a6afc30d6ab51cb6dd6613
Cole Fillinger <eloc574@gmail.com>
2023-05-10 15:40:19 -0400

    added GetUsers

---
+++ users_types.go
@@ -0,0 +1,39 @@
+package cybergo
+
+type GetUsersOptions struct {
+	ExtendedDetails *bool
+	Search          *string
+	Sort            *[]string
+	UserName        *string
+	UserType        *string
+	ComponentUser   *bool
+}
+
+type UserDetails struct {
+	ID                 int                       `json:"id,omitempty"`
+	Username           string                    `json:"username"`
+	Source             string                    `json:"source,omitempty"`
+	UserType           string                    `json:"userType,omitempty"`
+	ComponentUser      bool                      `json:"componentUser,omitempty"`
+	GroupsMembership   []*GroupMembershipDetails `json:"groupsMembership,omitempty"`
+	UserDN             string                    `json:"userDN,omitempty"`
+	VaultAuthorization []string                  `json:"vaultAuthorization,omitempty"`
+	Location           string                    `json:"location,omitempty"`
+	PersonalDetails    *UserPersonalDetails      `json:"personalDetails,omitempty"`
+	EnableUser         bool                      `json:"enableUser,omitempty"`
+	Suspended          bool                      `json:"suspended,omitempty"`
+}
+
+type GroupMembershipDetails struct {
+	GroupID   int    `json:"groupID,omitempty"`
+	GroupName string `json:"groupName,omitempty"`
+	GroupType string `json:"groupType,omitempty"`
+}
+
+type UserPersonalDetails struct {
+	FirstName    string `json:"firstName,omitempty"`
+	MiddleName   string `json:"middleName,omitempty"`
+	LastName     string `json:"lastName,omitempty"`
+	Organization string `json:"organization,omitempty"`
+	Department   string `json:"department,omitempty"`
+}
