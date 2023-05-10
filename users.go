6e217c859dc3ff508823e698e3ca39caef86fbb3
Cole Fillinger <eloc574@gmail.com>
2023-05-10 16:19:40 -0400

    rewrite GetUsers options to functional options pattern

--- users.go
+++ users.go
@@ -3,47 +3,16 @@ package cybergo
 import (
 	"encoding/json"
 	"log"
-	"net/url"
-	"strconv"
-	"strings"
 )
 
-func (p *PVWA) GetUsers(options *GetUsersOptions) ([]*UserDetails, error) {
-	path, err := url.Parse("API/Users")
-	if err != nil {
-		return nil, err
-	}
-
+func (p *PVWA) GetUsers(options ...GetUsersOption) ([]*UserDetails, error) {
 	var users []*UserDetails
 
-	if options != nil {
-		if options.ExtendedDetails != nil {
-			path.Query().Add("ExtendedDetails", strconv.FormatBool(*options.ExtendedDetails))
-		}
-		if options.Search != nil {
-			path.Query().Add("search", *options.Search)
-		}
-		if options.Sort != nil {
-			path.Query().Add("sort", strings.Join(*options.Sort, ","))
-		}
-		if options.UserName != nil {
-			path.Query().Add("userName", *options.UserName)
-		}
-		if options.UserType != nil {
-			path.Query().Add("userType", *options.UserType)
-		}
-		if options.ComponentUser != nil {
-			path.Query().Add("componentUser", strconv.FormatBool(*options.ComponentUser))
-		}
+	path := buildGetUsersPath(options...)
 
-		path.RawQuery = path.Query().Encode()
-	}
+	log.Println(path)
 
-	_path := path.String()
-
-	log.Println(_path)
-
-	res, err := p.Get(_path)
+	res, err := p.Get(path.String())
 	if err != nil {
 		return nil, err
 	}
--- users_types.go
+++ users_types.go
@@ -1,13 +1,10 @@
 package cybergo
 
-type GetUsersOptions struct {
-	ExtendedDetails *bool
-	Search          *string
-	Sort            *[]string
-	UserName        *string
-	UserType        *string
-	ComponentUser   *bool
-}
+import (
+	"net/url"
+	"strconv"
+	"strings"
+)
 
 type UserDetails struct {
 	ID                 int                       `json:"id,omitempty"`
@@ -36,4 +33,57 @@ type UserPersonalDetails struct {
 	LastName     string `json:"lastName,omitempty"`
 	Organization string `json:"organization,omitempty"`
 	Department   string `json:"department,omitempty"`
+}
+
+type GetUsersOption func(o *url.URL)
+
+func buildGetUsersPath(options ...GetUsersOption) *url.URL {
+	path, err := url.Parse("API/Users")
+	if err != nil {
+		return nil
+	}
+
+	for _, option := range options {
+		option(path)
+	}
+
+	path.RawQuery = path.Query().Encode()
+
+	return path
+}
+
+func WithExtendedDetails(b bool) GetUsersOption {
+	return func(o *url.URL) {
+		o.Query().Add("ExtendedDetails", strconv.FormatBool(b))
+	}
+}
+
+func WithSearch(s string) GetUsersOption {
+	return func(o *url.URL) {
+		o.Query().Add("search", s)
+	}
+}
+
+func WithSort(s []string) GetUsersOption {
+	return func(o *url.URL) {
+		o.Query().Add("sort", strings.Join(s, ","))
+	}
+}
+
+func WithUserName(s string) GetUsersOption {
+	return func(o *url.URL) {
+		o.Query().Add("userName", s)
+	}
+}
+
+func WithUserType(s string) GetUsersOption {
+	return func(o *url.URL) {
+		o.Query().Add("userType", s)
+	}
+}
+
+func WithComponentUser(b bool) GetUsersOption {
+	return func(o *url.URL) {
+		o.Query().Add("componentUser", strconv.FormatBool(b))
+	}
 }
